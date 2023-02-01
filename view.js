01258b734adb4b096ad00e723209fd31f82db592
Hashem <hashemabualteen@gmail.com>
2023-02-01 11:51:05 +0200

    added action to start fight button

--- view.js
+++ view.js
@@ -1,2 +1,8 @@
 const attackButton = document.getElementById("attack-button");
 const runButton = document.getElementById("run-button");
+const startFightButton = document.getElementById("start-fight-button");
+
+startFightButton.addEventListener("click" , ()=> {
+    // for now there is only one player and one monster
+    startFight(player , monster1);
+})
