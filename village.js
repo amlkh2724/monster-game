46a4f1bb3a704c7d5ee38295ff3a3a7f3f8f8f64
amal khanjer <amlkh2724@gmail.com>
2023-02-01 16:33:32 +0200

    fixed village.js - adding some css

--- village.js
+++ village.js
@@ -1,49 +1,29 @@
-const villageName = document.querySelector(".village-name")
-const monster1 = document.querySelector(".monsters1")
-const monster2 = document.querySelector(".monsters2")
-const monster3 = document.querySelector(".monsters3")
-const monster4 = document.querySelector(".monsters4")
-const monster5 = document.querySelector(".monsters5")
+const villageName = document.querySelector(".village-name");
+const monster1 = document.querySelector(".monsters1");
+const monster2 = document.querySelector(".monsters2");
+const monster3 = document.querySelector(".monsters3");
+const data = JSON.parse(localStorage.getItem("rpg-game-data"));
 
-const data =JSON.parse(localStorage.getItem("rpg-game-data"))
+const villagePicked = localStorage.getItem("village-picked");
+const village = data.locations[villagePicked];
 
-const villagePicked = localStorage.getItem("village-picked")
+villageName.innerText += village.name;
 
-const village=data.locations[villagePicked]
+monster1.innerHTML = `<div>${village.monsters[0].name}</div><div>${village.monsters[0].gold}</div><button  class="choose-monster-button">Choose</button>`;
+monster2.innerHTML = `<div>${village.monsters[1].name}</div><div>${village.monsters[1].gold}</div><button class="choose-monster-button">Choose</button>`;
+monster3.innerHTML = `<div>${village.monsters[2].name}</div><div>${village.monsters[2].gold}</div><button class="choose-monster-button">Choose</button>`;
 
-villageName.innerHTML = village.name
+const chooseMonsterButtons = document.querySelectorAll(".choose-monster-button");
 
-monster1.innerHTML = `${village.monsters[0].name},${village.monsters[0].gold}`
-monster2.innerHTML = `${village.monsters[1].name},${village.monsters[1].gold}`
-monster3.innerHTML = `${village.monsters[2].name},${village.monsters[2].gold}`
-// monster4.innerHTML = `monster:${village.monsters[3].name},gold:${village.monsters[3].gold}`
-// monster5.innerHTML = `monster:${village.monsters[4].name},gold:${village.monsters[4].gold}`
+for (let i = 0; i < chooseMonsterButtons.length; i++) {
+    chooseMonsterButtons[i].addEventListener("click", function () {
+        handleMonsterClick(i);
+    });
+}
 
-monster1.addEventListener("click", function () {
-    localStorage.setItem("monsterName", village.monsters[0])
+function handleMonsterClick(index) {
+    console.log("Monster chosen:", village.monsters[index].name);
+    localStorage.setItem("monsterName", JSON.stringify(village.monsters[index]));
     location.href = './index.html';
-})
+}
 
-monster2.addEventListener("click", function () {
-    localStorage.setItem("monsterName", village.monsters[1])
-
-    location.href = './index.html';
-})
-
-monster3.addEventListener("click", function () {
-    localStorage.setItem("monsterName", village.monsters[2])
-
-    location.href = './index.html';
-})
-
-// monster4.addEventListener("click", function () {
-//     localStorage.setItem("monsterName", village.monsters[3])
-
-//     location.href = './index.html';
-// })
-
-// monster5.addEventListener("click", function () {
-//     localStorage.setItem("monsterName", data.monsters[4])
-
-//     location.href = './index.html';
-// })
